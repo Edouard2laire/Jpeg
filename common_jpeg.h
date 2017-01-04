1cce561be966ab9ac121a8fcff50eea2aa90f819
antopino79 <antoinepinocortes@yahoo.fr>
2017-01-04 16:12:07 +0100

    Add files via upload

--- common_jpeg.h
+++ common_jpeg.h
@@ -6,7 +6,23 @@
 
 #define Taille_IMG 64
 
-typedef struct jpegData JPEGDATA;
+#define intoYCbCr {{0.257,-0.148,0.439},{0.504,-0.291,-0.368},{0.098,0.439,-0.071}}
+#define intoRGB {{1,1,1},{0,-0.34414,1.772},{1.402,-0.71414,0}}
+
+typedef struct jpegData {
+	int image[Taille_IMG][Taille_IMG][3];
+}JPEGDATA;
+
 typedef struct jpegData* pJPEGDATA;
 
+typedef struct pixel {
+	int pixel[3];
+}PIXEL;
+
+typedef struct pixel* pPIXEL;
+
 #endif /* COMMON_JPEG_H_ */
+
+void YCbCrintoRGB (JPEGDATA);
+void RGBintoYCbCr (JPEGDATA);
+void SubSampling (JPEGDATA);
--- fonctions.c
+++ fonctions.c
@@ -13,8 +13,50 @@
 #include "jpeg_header_parser.h"
 
 
-void RGBintoYCbCr (pJPEGDATA img){
+void RGBintoYCbCr (JPEGDATA img){
+	int i; int j; int k;
+	int R; int G; int B;
+	for (i=0;i<=Taille_IMG;i++){
+		for (j=0;j<=Taille_IMG;j++){
+			for (k=0;k<=2;k++){
+				R=img[i][j][0];
+				G=img[i][j][1];
+				B=img[i][j][2];
+				if (k==0){
+					img[i][j][k]=R*intoYCbCr[0][k]+G*intoYCbCr[1][k]+B*intoYCbCr[2][k]+16;
+				}else{
+					img[i][j][k]=R*intoYCbCr[0][k]+G*intoYCbCr[1][k]+B*intoYCbCr[2][k]+128;
+				}
+			}
+		}
+	}
+}
 
+void YCbCrintoRGB (JPEGDATA img){
+	int i ; int j ; int k;
+	int Y; int Cb; int Cr;
+	for (i=0;i<=Taille_IMG;i++){
+		for (j=0;j<=Taille_IMG;i++){
+			for (k=0;k<=2;k++){
+				Y=img[i][j][0];
+				Cb=img[i][j][1];
+				Cr=img[i][j][2];
+				img[i][j][k]=(Y-16)*intoRGB[0][k]+(Cb-128)*intoRBG[1][k]+(Cr-128)*intoRGB[2][k];
+				}
+			}
+		}
+	}
+
+
+void SubSampling_4_2_2 (JPEGDATA img){
+	int i; int j;
+	for (i=0;i<=Taille_IMG;i++){
+		for (j=0;j<=Taille_IMG;i++){
+			if (i%2==0){
+				free(img[i][j]);
+			}
+		}
+	}
 }
 
 
